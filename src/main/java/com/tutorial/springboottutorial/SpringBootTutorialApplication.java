9513a638002bce5941b35eea9701a70fef1394a7
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-20 18:21:17 -0500

    Simple API

--- src/main/java/com/tutorial/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/tutorial/springboottutorial/SpringBootTutorialApplication.java
@@ -1,13 +1,23 @@
 package com.tutorial.springboottutorial;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootTutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTutorialApplication.class, args);
+	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 
 }
