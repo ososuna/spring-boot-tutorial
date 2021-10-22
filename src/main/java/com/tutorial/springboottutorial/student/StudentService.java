515898e0ae00bd1b13811684bd543540974ffca6
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-22 01:52:56 -0500

    Create student repository

--- src/main/java/com/tutorial/springboottutorial/student/StudentService.java
+++ src/main/java/com/tutorial/springboottutorial/student/StudentService.java
@@ -1,24 +1,23 @@
 package com.tutorial.springboottutorial.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class StudentService {
-    
-    public List<Student> getStudents() {
-		return List.of(
-			new Student(
-                            1L,
-                            "Vally",
-                            "vally@test.com",
-                            LocalDate.of(2002, Month.DECEMBER, 9),
-                            18
-                        )
-		);
-	}    
+  
+  private final StudentRepository studentRepository;
+  
+  @Autowired
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
+  public List<Student> getStudents() {
+		return studentRepository.findAll();
+  }
 
 }
