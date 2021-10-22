d660331e04264f75ee53f5c8bfb32eac32a62fea
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-10-22 02:42:59 -0500

    Added email validation to register student

--- src/main/java/com/tutorial/springboottutorial/student/StudentService.java
+++ src/main/java/com/tutorial/springboottutorial/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.tutorial.springboottutorial.student;
  
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,16 @@ public class StudentService {
   }
 
   public void addNewStudent(Student student) {
-    System.out.println(student);
+    
+    Optional<Student> studentOptional = studentRepository
+        .findStudentByEmail(student.getEmail());
+
+    if (studentOptional.isPresent()) {
+      throw new IllegalStateException("Already existing email");
+    }
+
+    studentRepository.save(student);
+
   }
 
 }
